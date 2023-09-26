e19658b917c503563827db86b2491f599d4a1228
senakim11 <kjbsena.student@ua.edu.ph>
2023-09-26 11:41:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function CalcButton({label}){
+  return (
+    <button>
+      {label}
+    </button>
+  )
+}
+
+function CalcDisplay({display}) {
+  return (
+    <div className="CalcDisplay">
+      {display}
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="CalcContainer">
+      <CalcDisplay display={"Hello World"}/>
+    <div className="ButtonContainer">
+     <CalcButton label={7}/>
+     <CalcButton label={8}/>
+     <CalcButton label={9}/>
+     <CalcButton label={"+"}/>
+     <CalcButton label={4}/>
+     <CalcButton label={5}/>
+     <CalcButton label={6}/>
+     <CalcButton label={"-"}/>
+     <CalcButton label={1}/>
+     <CalcButton label={2}/>
+     <CalcButton label={3}/>
+     <CalcButton label={"*"}/>
+     <CalcButton label={"C"}/>
+     <CalcButton label={0}/>
+     <CalcButton label={"="}/>
+     <CalcButton label={"÷"}/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
